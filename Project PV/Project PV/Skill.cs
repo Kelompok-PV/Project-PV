43780dc4496eef05eca03b74a7e73e4fd5f6a495
Yongki <yongkitanu@gmail.com>
2019-11-01 00:07:37 +0700

    Update Skill.cs

--- Project PV/Project PV/Skill.cs
+++ Project PV/Project PV/Skill.cs
@@ -1,28 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Project_PV
 {
-    public abstract class Skill
+    class skill
     {
-        public string name { get; set; }
-        public skillEffect effect { get; set; }
-        public int damage { get; set; }
-        public Image skillIcon { get; set; }
-    }
-    class Healing : Skill
-    {
+        public string nama { get; set; }
+        public efek skill_efek { get; set; }
+        public int[] posisi_skill{ get; set; }
+        public status status_skill{ get; set; }
 
-    }
-
-    public enum skillEffect
-    {
-        poison,
-        bleed,
-        targeted
+        enum efek
+        {
+            bless
+        }
     }
 }
